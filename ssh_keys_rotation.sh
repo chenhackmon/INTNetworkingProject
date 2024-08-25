62876cac3318f5a1f83dee7069bdac3f6e8c84f4
chen <chenhackmon@gmail.com>
2024-08-25 14:21:37 +0300

    P

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -13,14 +13,17 @@ PUBLIC_KEY_PATH="$NEW_KEY_PATH.pub"
 OLD_KEY_PATH="$HOME/.ssh/id_rsa"
 
 # Generate a new SSH key pair
+echo "Generating new SSH key pair..."
 ssh-keygen -t rsa -b 4096 -f $NEW_KEY_PATH -N ""
 chmod 600 $NEW_KEY_PATH
 
 # Copy the new public key to the authorized_keys on the private instance
 NEW_PUBLIC_KEY=$(cat $PUBLIC_KEY_PATH)
+echo "Adding new public key to the private instance..."
 ssh -i "$OLD_KEY_PATH" ubuntu@$PRIVATE_IP "echo '$NEW_PUBLIC_KEY' >> ~/.ssh/authorized_keys"
 
 # Verify the new key works
+echo "Verifying new key..."
 ssh -i "$NEW_KEY_PATH" ubuntu@$PRIVATE_IP 'exit'
 if [ $? -ne 0 ]; then
   echo "Failed to connect to the private instance using the new key."
@@ -30,10 +33,11 @@ fi
 # Remove the old key from authorized_keys on the private instance
 OLD_PUBLIC_KEY=$(cat $OLD_KEY_PATH.pub)
 ESCAPED_OLD_KEY=$(echo "$OLD_PUBLIC_KEY" | sed 's/[\/&]/\\&/g')
+echo "Removing old public key from the private instance..."
 ssh -i "$NEW_KEY_PATH" ubuntu@$PRIVATE_IP "sed -i '/$ESCAPED_OLD_KEY/d' ~/.ssh/authorized_keys"
-#ssh -i "$NEW_KEY_PATH" ubuntu@$PRIVATE_IP "grep -v '$OLD_PUBLIC_KEY' ~/.ssh/authorized_keys > ~/.ssh/authorized_keys.tmp && mv ~/.ssh/authorized_keys.tmp ~/.ssh/authorized_keys"
 
 # Verify the old key no longer works
+echo "Verifying old key removal..."
 ssh -i "$OLD_KEY_PATH" ubuntu@$PRIVATE_IP 'exit'
 if [ $? -eq 0 ]; then
   echo "Old key is still valid, which shouldn't be the case."
@@ -41,9 +45,11 @@ if [ $? -eq 0 ]; then
 fi
 
 # Remove old key from the public instance
+echo "Cleaning up old keys..."
 rm -f $OLD_KEY_PATH $OLD_KEY_PATH.pub
 
 # Replace the old key with the new key locally
+echo "Updating local keys..."
 mv $NEW_KEY_PATH $HOME/.ssh/id_rsa
 mv $PUBLIC_KEY_PATH $HOME/.ssh/id_rsa.pub
 
