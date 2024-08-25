93eacd744e2ee3e0fd93813ea042f742dece508d
chen <chenhackmon@gmail.com>
2024-08-25 14:24:42 +0300

    P

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -1,56 +1,45 @@
 #!/bin/bash
 
-# Ensure correct usage
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <private-instance-ip>"
-  exit 1
+if [ -z "$1" ]; then
+   echo "please provide private-instance-ip"
+   exit 1
 fi
 
-# Variables
-PRIVATE_IP=$1
-NEW_KEY_PATH="$HOME/.ssh/id_rsa_new"
-PUBLIC_KEY_PATH="$NEW_KEY_PATH.pub"
-OLD_KEY_PATH="$HOME/.ssh/id_rsa"
+PRIVATE_INSTANCE_IP="$1"
+NEW_KEY_NAME="id_rsa"
+NEW_KEY_PATH="$HOME/.ssh/$NEW_KEY_NAME"
+OLD_KEY_PATH="$NEW_KEY_PATH"
+mv "$OLD_KEY_PATH" "$OLD_KEY_PATH".old
+OLD_KEY_PATH="$OLD_KEY_PATH".old
+OLD_KEY_PATH_PUB="$HOME/.ssh/$NEW_KEY_NAME.pub"
+echo "$OLD_KEY_PATH_PUB"
+mv "$OLD_KEY_PATH_PUB" "$OLD_KEY_PATH_PUB".old
+OLD_KEY_PATH_PUB="$OLD_KEY_PATH_PUB".old
+echo "$OLD_KEY_PATH_PUB"
 
-# Generate a new SSH key pair
-echo "Generating new SSH key pair..."
-ssh-keygen -t rsa -b 4096 -f $NEW_KEY_PATH -N ""
-chmod 600 $NEW_KEY_PATH
-
-# Copy the new public key to the authorized_keys on the private instance
-NEW_PUBLIC_KEY=$(cat $PUBLIC_KEY_PATH)
-echo "Adding new public key to the private instance..."
-ssh -i "$OLD_KEY_PATH" ubuntu@$PRIVATE_IP "echo '$NEW_PUBLIC_KEY' >> ~/.ssh/authorized_keys"
-
-# Verify the new key works
-echo "Verifying new key..."
-ssh -i "$NEW_KEY_PATH" ubuntu@$PRIVATE_IP 'exit'
-if [ $? -ne 0 ]; then
-  echo "Failed to connect to the private instance using the new key."
-  exit 1
+if ! ssh-keygen -t rsa -b 2048 -f "$NEW_KEY_PATH" -N ""; then
+   echo "Failed to generate SSH key"
+   exit 1
 fi
-
-# Remove the old key from authorized_keys on the private instance
-OLD_PUBLIC_KEY=$(cat $OLD_KEY_PATH.pub)
-ESCAPED_OLD_KEY=$(echo "$OLD_PUBLIC_KEY" | sed 's/[\/&]/\\&/g')
-echo "Removing old public key from the private instance..."
-ssh -i "$NEW_KEY_PATH" ubuntu@$PRIVATE_IP "sed -i '/$ESCAPED_OLD_KEY/d' ~/.ssh/authorized_keys"
-
-# Verify the old key no longer works
-echo "Verifying old key removal..."
-ssh -i "$OLD_KEY_PATH" ubuntu@$PRIVATE_IP 'exit'
-if [ $? -eq 0 ]; then
-  echo "Old key is still valid, which shouldn't be the case."
-  exit 1
+echo "$NEW_KEY_PATH"
+if ! sudo chmod 400 "$NEW_KEY_PATH"; then
+   echo "Failed to set permissions on the key"
+   exit 1
 fi
+echo "$OLD_KEY_PATH"
+echo "$NEW_KEY_PATH"
 
-# Remove old key from the public instance
-echo "Cleaning up old keys..."
-rm -f $OLD_KEY_PATH $OLD_KEY_PATH.pub
+if ! ssh -i "$OLD_KEY_PATH" "ubuntu@$PRIVATE_INSTANCE_IP" "cat > ~/.ssh/authorized_keys" < "$NEW_KEY_PATH.pub"; then
+   echo "Failed to copy key to the private machine"
+   exit 1
+fi
+echo "Testing SSH connection with the new key..."
+if ! ssh -i "$NEW_KEY_PATH" "ubuntu@$PRIVATE_INSTANCE_IP" -o StrictHostKeyChecking=no "echo 'Connection successful with new key'"; then
+    echo "Failed to connect with the new key"
+    exit 1
+fi
+echo "$NEW_KEY_PATH"
+echo "$OLD_KEY_PATH"
 
-# Replace the old key with the new key locally
-echo "Updating local keys..."
-mv $NEW_KEY_PATH $HOME/.ssh/id_rsa
-mv $PUBLIC_KEY_PATH $HOME/.ssh/id_rsa.pub
-
-echo "SSH key rotation completed successfully."
+echo "Key rotation successful. You can now use the new key to access the private instance."
+rm -f "$OLD_KEY_PATH" "$OLD_KEY_PATH_PUB"
